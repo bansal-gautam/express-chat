9dd34d1bf1543083bca6eda5966928db986ec951
Gautam Bansal <119778021+bansal-gautam@users.noreply.github.com>
2023-05-16 16:37:29 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(3000, (req, res) => {
+    console.log("App running on port 3000")
+})
